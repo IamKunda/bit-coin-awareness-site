e6a1fee6d9f113d344fe5dac6321484429d6b572
Kunda Sakala <iamkundasakala@gmail.com>
2022-03-08 15:12:08 +0200

    The best Semi-full Stack dev my gee

--- src/Pages/Donate.js
+++ src/Pages/Donate.js
@@ -1,13 +1,32 @@
 //Component Imports
 import TopNav from '../Components/TopNav';
+import Footer from '../Components/Footer';
+import MyForm from '../Components/MyForm';
 
-function Donate(){
-    return(
+function Donate() {
+    return (
         <div>
-            <TopNav/>
-            <p>
-                This is the Donate Page!
-            </p>
+            <TopNav />
+            <div className='container donate-container'>
+                <p className='h2 text-decoration-underline'>Please Support Our Work</p>
+                <div className='row'>
+                    <div className='col'>
+                        <MyForm />
+                        <button className='btn btn-warning'>Donate</button>
+                    </div>
+                    <div className='col'>
+                        <div className="input-group mb-3">
+                            <span className="input-group-text">K</span>
+                            <span className="input-group-text">0.00</span>
+                            <input type="number" class="form-control" id='user-donation' />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div className='custom-container'>
+                <Footer />
+            </div>
         </div>
     )
 }
